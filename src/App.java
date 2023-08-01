cfbb104878dba05a7c38f5c512b6fd0f1cc4cf12
marco <antonioborges.marco@gmail.com>
2023-08-01 09:34:51 -0300

    Aplicação Primeira Thread

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ImprimeString imprimeString = new ImprimeString();
+        Thread thread = new Thread(imprimeString, "Eita");
+        thread.start();
     }
 }
---
+++ src/ImprimeString.java
@@ -0,0 +1,8 @@
+public class ImprimeString implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Eita como programa");
+    }
+    
+}
